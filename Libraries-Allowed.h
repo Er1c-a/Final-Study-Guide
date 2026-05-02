54c094f2cef403cc8bcb7a18d51d91ea49a0514a
Er1c-a <eric.advincula572@gmail.com>
2026-05-02 16:12:13 -0400

    Updated

---
+++ Libraries-Allowed.h
@@ -0,0 +1,10 @@
+#include <iostream> //cout and cin
+#include <vector>
+#include <string>
+#include <cctype> //tolower
+#include <limits> 
+#include <iomanip> // std::setw, std::setprecision
+using namespace std;
+#include <sstream> //String streaming
+#include <fstream> //File reading
+#include <stdexcept> //Allows error throwing
